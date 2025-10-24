acb6dfa12ebb5b4d4f27195e9115cc04377307e7
Vendouple <soupnoodle.buystuffhere@gmail.com>
2025-10-24 19:21:35 +0700

    Changed the lastfm autoplay to autoplay engine v1. V2 will be made soon. This is so that users (if they like) can choose between v1 or v2. If most users prefer v2, v1 will then get deprecated (still available as optional download)

--- modules/music/music_player.py
+++ modules/music/music_player.py
@@ -28,7 +28,7 @@ from modules.music.lavalink.manager import (
 
 # Import Last.fm autoplay
 try:
-    from modules.music.lastfm_autoplay import get_lastfm_autoplay
+    from modules.music.autoplayengine_v1 import get_lastfm_autoplay
 
     LASTFM_AUTOPLAY_AVAILABLE = True
 except ImportError:
